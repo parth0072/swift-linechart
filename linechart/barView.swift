a174b2f878f8b5e286f2a4c96ce558d6f9af0fec
Parth <parth0072@live.com>
2018-07-05 11:59:06 +0530

    - updated

---
+++ linechart/barView.swift
@@ -0,0 +1,17 @@
+//
+//  barView.swift
+//  linechart
+//
+//  Created by Mobilefirst on 7/4/18.
+//  Copyright © 2018 zemirco. All rights reserved.
+//
+
+import UIKit
+
+class barView: UIView {
+
+    @IBOutlet var fHeight: NSLayoutConstraint!
+    
+    @IBOutlet var sHeight: NSLayoutConstraint!
+    
+}
